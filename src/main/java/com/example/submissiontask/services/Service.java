0f6b8d71deecdac0c7e257077b5d7253358888ed
Daabas21 <hassandaabas@hotmail.com>
2023-01-24 00:52:42 +0100

    red login

---
+++ src/main/java/com/example/submissiontask/services/Service.java
@@ -0,0 +1,9 @@
+package com.example.submissiontask.services;
+
+public class Service {
+
+
+    public boolean login(String username, String password) {
+        return false;
+    }
+}
---
+++ src/test/java/com/example/submissiontask/services/ServiceTest.java
@@ -0,0 +1,23 @@
+package com.example.submissiontask.services;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ServiceTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"hassan, pass", "pavel, password", "viktor, pass"})
+    public void checkUsernamePasswordLoginAuthorization(String username, String password){
+        //Given
+        Service service = new Service();
+
+        //When
+        boolean isLogged = service.login(username, password);
+
+        //Then
+        assertTrue(isLogged);
+    }
+
+}
