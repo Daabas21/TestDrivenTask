017a6e1fc9f37c9857b82c6d21a8413779067de0
Daabas21 <hassandaabas@hotmail.com>
2023-01-25 12:28:19 +0100

    red assert Throw fails(same user's properties)

--- src/test/java/com/example/submissiontask/services/ServiceTest.java
+++ src/test/java/com/example/submissiontask/services/ServiceTest.java
@@ -5,7 +5,6 @@ import com.example.submissiontask.Repo.AppUserRepo;
 import com.example.submissiontask.entity.AppUser;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
@@ -49,6 +48,7 @@ public class ServiceTest {
         //Then
         verify(appUserRepo, times(2)).findByUsernameAndPassword(alex);
         assertEquals(alex, appUser);
+        assertThrows(LoginException.class, () -> service.findByUsernameAndPassword(new AppUser("Alex", "pass")));
     }
 
     @Test
